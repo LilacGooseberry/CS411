4c496df63bc510dc2f3991b39a3a3cd1eee23a5a
sicong <sicongh3@illinois.edu>
2020-11-27 18:15:12 -0600

    update home page

--- gun/gun/settings.py
+++ gun/gun/settings.py
@@ -78,12 +78,6 @@ WSGI_APPLICATION = 'gun.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'OPTIONS': {
-            'read_default_file': '/etc/mysql/my.cnf',
-        },
-    },
-    'product_db': {
       'ENGINE' : 'djongo',
       "CLIENT" : {
         "name" : 'DarkDB',
